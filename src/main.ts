66db8ca7467fc0a6cfe821151bed892eaf07634e
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-20 15:39:58 -0400

    feat:add validate class

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule } from '@nestjs/swagger';
 import { configsSwagger } from './docs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const document = SwaggerModule.createDocument(app, configsSwagger);
+  // validaciones
+  app.useGlobalPipes(new ValidationPipe());
   
+  // docs
+  const document = SwaggerModule.createDocument(app, configsSwagger);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(configService.get<number>('port'));
