ce6414aa3a757477064706fa617cd10008d8e147
William Mosquera <wjmmk3@gmail.com>
2024-07-24 15:57:33 -0500

    feat:coupon-movie-user modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
+import { CouponModule } from './coupon/coupon.module';
 import configuration from './config/configuration';
 
 @Module({
   imports: [
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    UserModule,
+    MovieModule,
+    CouponModule,
   ],
   providers: [AppService],
 })
