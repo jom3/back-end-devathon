31518a7b08a41b2148f43b8a861d08a2778b7cd1
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-30 21:49:52 -0400

    feat:updatr

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,17 @@ import { configsSwagger } from './docs';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   // validaciones
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }));
-  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   // docs
   const document = SwaggerModule.createDocument(app, configsSwagger);
   SwaggerModule.setup('docs', app, document);
