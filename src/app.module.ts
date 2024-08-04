db5d7d05fb9b1e0f265dc7ffec7b4efbea628a02
William Mosquera <wjmmk3@gmail.com>
2024-08-03 23:48:29 -0500

    feat: app_module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,19 @@ import { CouponModule } from './coupon/coupon.module';
 import configuration from './config/configuration';
 import { PrismaService } from './prisma/prisma.service';
 import { CouponService } from './coupon/coupon.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [
     PrismaModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     UserModule,
     MovieModule,
     CouponModule,
   ],
-  providers: [AppService, PrismaService, CouponService],
+  providers: [AppService, PrismaService, CouponService, AuthService, UserService],
 })
 export class AppModule {}
