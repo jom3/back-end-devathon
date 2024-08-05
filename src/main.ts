35737de5bfd211a95fa5ea735983f1337a90c2a1
William Mosquera <wjmmk3@gmail.com>
2024-08-05 14:35:52 -0500

    feat: prefix_path_proposal

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,12 @@ async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  // validaciones
+
+   /* Prefix
+     app.setGlobalPrefix('api');
+  */
+
+  // validaciones de los DTOs.
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,7 +22,7 @@ async function bootstrap() {
     }),
   );
 
-  // docs
+   // docs
   const document = SwaggerModule.createDocument(app, configsSwagger);
   SwaggerModule.setup('docs', app, document);
 
