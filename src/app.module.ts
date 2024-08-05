1f2c25ebb16ed2711af0315f273d4aa3c6db533d
William Mosquera <wjmmk3@gmail.com>
2024-08-05 06:35:10 -0500

    fix: movie_module_deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { MovieModule } from './movie/movie.module';
 import { CouponModule } from './coupon/coupon.module';
 import configuration from './config/configuration';
 import { PrismaService } from './prisma/prisma.service';
@@ -19,7 +17,6 @@ import { UserService } from './user/user.service';
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     UserModule,
-    MovieModule,
     CouponModule,
   ],
   providers: [AppService, PrismaService, CouponService, AuthService, UserService],
