c3268cd786b0e01705de38307f1b210475ef553a
William Mosquera <wjmmk3@gmail.com>
2024-08-05 14:38:18 -0500

    feat: pagination_dto_added

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from "class-transformer";
+import { IsOptional, IsPositive } from "class-validator";
+
+export class PaginationDto {
+
+@IsPositive()
+@IsOptional()
+@Type(() => Number)
+readonly page?: number = 1;
+
+@IsPositive()
+@IsOptional()
+@Type(() => Number)
+readonly limit?: number = 10;
+
+}
