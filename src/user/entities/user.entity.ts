a582d3fc3b8a279697a157bb519146d60eb035d2
William Mosquera <wjmmk3@gmail.com>
2024-08-15 22:15:22 -0500

    fix: user_entity and create-user.dto updated

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,9 @@
-export class User {}
+export class User {
+    public dni?: string;
+    public fullName: string;
+    public phone?: string;
+    public genre: string;
+    public country: string;
+    public email: string;
+    public password: string;
+}
